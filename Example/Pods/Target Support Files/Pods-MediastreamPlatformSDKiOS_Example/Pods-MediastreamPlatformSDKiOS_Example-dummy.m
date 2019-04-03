8dfb1c74beca34db3e638c7cd72206a9df474d13
cruiz <ruizcarlos1984@gmail.com>
2019-04-03 16:12:06 -0300

    Create project via Pod

---
+++ Example/Pods/Target Support Files/Pods-MediastreamPlatformSDKiOS_Example/Pods-MediastreamPlatformSDKiOS_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MediastreamPlatformSDKiOS_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MediastreamPlatformSDKiOS_Example
+@end
