17b37f62d2c0efd8a9c957f1191686c4f3008acc
cruiz <ruizcarlos1984@gmail.com>
2019-04-03 16:08:52 -0300

    Initial commit

---
+++ Example/MediastreamPlatformSDKiOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MediastreamPlatformSDKiOS
+//
+//  Created by cruiz666 on 04/03/2019.
+//  Copyright (c) 2019 cruiz666. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
